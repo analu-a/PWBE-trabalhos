7958f3845061977947d4d6905a4cf7e7e7b376a9
Ana Luiza <analuiza.azevedomorais@gmail.com>
2023-09-15 12:26:59 -0300

    terminar imc

---
+++ Atividades/exercício01/app.js
@@ -0,0 +1,18 @@
+/* ************************************************************** *
+* Objetivo: Fazer uma calculadora IMC e classificar os valores    *
+* Data: 15/09/2023                                                *
+* Autor: Ana Luiza                                                *
+* Versão: 1.0                                                     *
+* *************************************************************** */ 
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+entradaDados.question('Digite seu peso', function(valorPeso){
+    let peso = valorPeso
+
+    
+})
+
---
+++ Atividades/exercício01/modulo/imc.js
@@ -0,0 +1,43 @@
+const calculoImc = function (peso, altura){
+
+    let valorPeso = String(peso).replace(',' , '.')
+    let valorAltura = String(altura).replace(',' , '.')
+
+    let resultadoImc 
+   
+
+   resultadoImc = Number(valorPeso) / (Number(valorAltura) * Number(valorAltura))
+
+   
+   return resultadoImc
+    
+} 
+
+
+const validacaoPeso = function(resultadoImc){
+
+    let situacaoImc
+
+    if(resultadoImc < 18.5){
+        situacaoImc = console.log('Você está abaixo do peso')
+     
+        }else if(resultadoImc >= 18.5 && resultadoImc <= 24.9){
+            situacaoImc = 'Você está com o peso normal'
+     
+        }else if(resultadoImc >= 25 && resultadoImc <= 29,9){
+            situacaoImc = 'Você está acima do peso'
+     
+        }else if(resultadoImc >= 30 && resultadoImc <= 34.9){
+            situacaoImc = 'Você está com obesidade I'
+     
+        }else if(resultadoImc >= 35 && resultadoImc <= 39.9){
+            situacaoImc ='Você está com obesidade II'
+     
+        }else if(resultadoImc >= 40){
+            situacaoImc ='Você está com obesidade III'
+        }
+        return situacaoImc
+}
+
+console.log(calculoImc('50' , '2,0')), console.log(validacaoPeso('18.5'))
+
